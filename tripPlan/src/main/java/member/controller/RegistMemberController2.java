8a41c36c8dabb7f048fcf0d1dbb34e57dba4dea5
strepituss <strepituss@naver.com>
2016-12-20 23:54:14 +0900

    회원가입 DB 수정

--- tripPlan/src/main/java/member/controller/RegistMemberController2.java
+++ tripPlan/src/main/java/member/controller/RegistMemberController2.java
@@ -48,10 +48,8 @@ public class RegistMemberController2 {
 	@RequestMapping(value = "/mypage.do", method = RequestMethod.POST)
 	public String submit(@ModelAttribute MemberInfo memberInfo, BindingResult result, Model model,
 			@RequestParam("uploadImg") MultipartFile uploadImg) {
-		System.out.println(memberInfo.getM_email());
-		System.out.println(memberInfo.getM_pass());
 		new MemberInfoValidator2().validate(memberInfo, result);
-	
+
 		// 오류 발생 시 에러 메세지 처리
 		if (result.hasErrors()) {
 
@@ -62,17 +60,16 @@ public class RegistMemberController2 {
 
 		upload(uploadImg, memberInfo);
 
-		
 		return "myPage";
 	}
 
 	void upload(MultipartFile uploadImg, MemberInfo memberInfo) {
 		// 파일사이즈가 0일 경우 (업로드를 하지 않았을 경우) image폴더에 임시파일을 생성하지 않음
 		if (uploadImg.getSize() != 0.0) {
-			
-				File uploadFile = new File(
-						"c://file//" + System.currentTimeMillis() + "-" + uploadImg.getOriginalFilename());
-			try {	
+
+			File uploadFile = new File(
+					"c://images//" + System.currentTimeMillis() + "-" + uploadImg.getOriginalFilename());
+			try {
 				uploadImg.transferTo(uploadFile);
 			} catch (IllegalStateException e) {
 				// TODO Auto-generated catch block
@@ -83,7 +80,14 @@ public class RegistMemberController2 {
 			}
 
 			memberInfo.setM_profile(uploadFile.getPath());
-
+			
+			int check = dao.insertFile(memberInfo);
+			if (check == 0)
+				System.out.println("업로드 실패");
+		}  
+		
+		if(memberInfo.getM_profile()==null) {
+			memberInfo.setM_profile("");
 			int check = dao.insertFile(memberInfo);
 			if (check == 0)
 				System.out.println("업로드 실패");
--- tripPlan/src/main/java/member/dao/MemberDAO.java
+++ tripPlan/src/main/java/member/dao/MemberDAO.java
@@ -6,13 +6,9 @@ import member.model.MemberInfo;
 public class MemberDAO extends SqlSessionDaoSupport {
 
 	public int insertFile(MemberInfo memberInfo) {
-		System.out.println(memberInfo.getM_email());
-		System.out.println(memberInfo.getM_pass());
-		System.out.println(memberInfo.getM_nickname());
-		System.out.println(memberInfo.getM_sex());
-		System.out.println(memberInfo.getM_agerange());
+
 		System.out.println(memberInfo.getM_profile());
-		System.out.println(memberInfo.getM_identified());
+		
 		
 		int check = getSqlSession().insert("member.regist", memberInfo);
 		return check;
