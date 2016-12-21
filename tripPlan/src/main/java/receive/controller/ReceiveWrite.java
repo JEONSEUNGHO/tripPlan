f988c065fd479df0d2cd180fe2c3e93b9c70f5e7
angel <angel@DESKTOP-4CMQAKF>
2016-12-21 16:10:38 +0900

    수정

---
+++ tripPlan/src/main/java/receive/controller/ReceiveWrite.java
@@ -0,0 +1,28 @@
+package receive.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import receive.DAO.ReceiveDataBean;
+import receive.Service.receiveService;
+
+@Controller
+@RequestMapping("/receive/write")
+public class ReceiveWrite {
+	@Autowired
+	private receiveService service;
+
+	public void setService(receiveService service) {
+		this.service = service;
+
+	}
+	@RequestMapping(value = "/writePro.do")
+	public String writePro(ReceiveDataBean receive, HttpServletRequest request) throws Throwable {
+		receive.setRm_sender(request.getRemoteAddr());
+		service.insertreceive(receive);
+		return "write";
+	}
+}
